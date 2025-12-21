566f5c64051aecced46ab26a62f26d0178bb7687
Andrei Chakhovich <cmak666@gmail.com>
2025-12-21 17:53:27 +0300

    music compression

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "122959697"
+    serviceWorkerVersion: "2025065772"
   }
 });
