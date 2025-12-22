cdbc62699133dc63e2fca22eaeb2eef891062ac8
Andrei Chakhovich <cmak666@gmail.com>
2025-12-22 20:39:54 +0300

    quiz

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1767112068"
+    serviceWorkerVersion: "4192413804"
   }
 });
